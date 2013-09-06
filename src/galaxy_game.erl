96fa274be1d96a1c35f14f42adb71d807558bd5e
Simon <simon@majou.org>
2013-09-06 12:02:40 +0200

    ok

--- src/galaxy_game.erl
+++ src/galaxy_game.erl
@@ -28,15 +28,71 @@
 
 -export([setup_universe/3, teardown_universe/1, simulate_attack/2]).
 
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_Type, {Planets, Shields, Alliances}) ->
+  setup_universe(Planets, Shields, Alliances).
+
+stop(Planets) ->
+  teardown_universe(Planets).
+
 %% @doc Set up a universe described by the input.
-%% The imput is asumed to be minimal and non redundant (i.e. if there is an
+%% The input is assumed to be minimal and non redundant (i.e. if there is an
 %% alliance {a, b} there won't be an alliance {b, a}).
 %% Once this function returns, the universe is expected to be fully ready,
 %% shields, alliances and all.
 -spec setup_universe([planet()], [shield()], [alliance()]) -> ok.
+
 %% @end
 setup_universe(Planets, Shields, Alliances) ->
-    unimplemented.
+  io:format("setup_universe ~p~n", [Planets]),
+  spawn_planets(Planets),
+  setup_shields(Shields),
+  link_planets(Alliances),
+  io:format("setup_universe end ~p~n", [Planets]),
+  ok.
+
+spawn_planets([]) ->
+  ok;
+spawn_planets([Planet|Planets]) ->
+  io:format("spawn ~p~n", [Planet]),
+  register(Planet, spawn(fun() -> shield(Planet) end)),
+  spawn_planets(Planets).
+
+shield(Planet) ->
+  receive
+    {ping, From, FromPlanet} ->
+      From ! {pong, FromPlanet},
+      shield(Planet);
+    {link, ToPlanet} ->
+      io:format("~p and ~p are linked~n", [Planet, ToPlanet]),
+      link(whereis(ToPlanet)),
+      shield(Planet);
+    shield ->
+      io:format("~p has a shield~n", [Planet]),
+      process_flag(trap_exit, true),
+      shield(Planet);
+    % Received only with the trap_exit
+    {'EXIT', _From, laser} ->
+      io:format("Laser was deflected from ~p~n", [Planet]),
+      shield(Planet);
+    {'EXIT', _From, Reason} ->
+      io:format("~p was destroyed by ~p~n", [Planet, Reason]),
+      exit(Reason)
+  end.
+link_planets([]) ->
+  ok;
+link_planets([{A, B}|Alliances]) ->
+  A ! {link, B},
+  link_planets(Alliances).
+
+setup_shields([]) ->
+  ok;
+setup_shields([Shield|Shields]) ->
+  Shield ! shield,
+  setup_shields(Shields).
 
 %% @doc Clean up a universe simulation.
 %% This function will only be called after calling setup_universe/3 with the
@@ -46,7 +102,16 @@ setup_universe(Planets, Shields, Alliances) ->
 -spec teardown_universe([planet()]) -> ok.
 %% @end
 teardown_universe(Planets) ->
-    unimplemented.
+  io:format("teardown_universe ~p~n", [Planets]),
+  terminate(Planets).
+
+terminate([]) ->
+  ok;
+terminate([Planet|Planets]) ->
+  io:format("terminate ~p~n", [Planet]),
+  PPid = whereis(Planet),
+  exit(PPid, shutdown),
+  terminate(Planets).
 
 %% @doc Simulate an attack.
 %% This function will only be called after setting up a universe with the same
@@ -54,6 +119,18 @@ teardown_universe(Planets) ->
 %% It returns the list of planets that have survived the attack
 -spec simulate_attack([planet()], [attack()]) -> Survivors::[planet()].
 %% @end
-simulate_attack(Planets, Actions) ->
-    unimplemented.
-
+simulate_attack(Planets, []) ->
+  timer:sleep(1),
+  lists:filter(fun(Planet) ->
+    Pid = whereis(Planet),
+    (Pid /= undefined) andalso is_process_alive(Pid)
+  end, Planets);
+simulate_attack(Planets, [{Attack, Planet}|Attacks]) ->
+  Pid = whereis(Planet),
+  case Pid of
+    undefined ->
+      simulate_attack(Planets, Attacks);
+    _ ->
+      exit(whereis(Planet), Attack)
+  end,
+  simulate_attack(Planets, Attacks).
--- test/galaxy_game_eunit.erl
+++ test/galaxy_game_eunit.erl
@@ -3,70 +3,45 @@
 -include_lib("eunit/include/eunit.hrl").
 
 galaxy_game_test_() ->
-    P = [mercury, venus, earth, mars],
-    S = [venus, earth],
-    As = [{mercury, venus}, {venus, earth}],
-    A = [{nuclear, venus}, {laser, earth}],
-    Exp = [earth, mars],
-    [
-        {setup, 
-            %Setup
-            fun () -> galaxy_game:setup_universe(P, S, As) end,
-            %Teardown
-            fun (ok) -> galaxy_game:teardown_universe(P) end,
-            %Tests
-            fun (ok) ->
-                    case is_valid_setup(P, S, As) of
-                        {true, true, true} ->
-                            ?_assertEqual(true, true);
-                        V ->
-                            validity_printout(V),
-                            ?assertEqual(true, false)
-                    end
-            end
-        },
-        {setup, 
-            %Setup
-            fun () -> galaxy_game:setup_universe(P, S, As) end,
-            %Teardown
-            fun (ok) -> galaxy_game:teardown_universe(P) end,
-            %Tests
-            fun (ok) -> [?_assertEqual(Exp, galaxy_game:simulate_attack(P, A))] end
-        }
-    ].
-
-
-%%==============================================================================
-%% Internal Functions
-%%==============================================================================
-
-is_valid_setup(Planets, Shields, Alliances) ->
-    VPlanets = valid_planets(Planets),
-    case VPlanets of
-        true -> {VPlanets, planets_shielded(Shields), allied(Alliances)};
-        false -> {false, untested, untested}
-    end.
+  Planets = [mercury, venus, earth, mars],
+  Shields = [venus, earth],
+  Alliances = [{mercury, venus}, {venus, earth}],
+  Attacks = [{nuclear, venus}, {laser, earth}],
+  Exp = [earth, mars],
+  {setup,
+    fun () -> galaxy_game:setup_universe(Planets, Shields, Alliances) end,
+    fun (ok) -> galaxy_game:teardown_universe(Planets) end,
+    fun (ok) ->
+      [?_assert(valid_planets(Planets)),
+       ?_assert(planets_shielded(Shields)),
+       ?_assert(allied(Alliances)),
+       ?_assertEqual(Exp, galaxy_game:simulate_attack(Planets, Attacks))]
+    end
+  }.
 
 valid_planets(Planets) ->
-    lists:all(fun (P) ->
-                PPid = whereis(P),
-                PPid /= undefined andalso erlang:is_process_alive(PPid)
-        end, Planets).
+  lists:all(fun (P) ->
+    PPid = whereis(P),
+    (PPid /= undefined) and is_process_alive(PPid)
+  end, Planets).
 
 planets_shielded(Shields) ->
-    lists:all(fun (S) ->
-                {trap_exit, true} == erlang:process_info(whereis(S), trap_exit)
-        end, Shields).
+  lists:all(fun (S) ->
+    {trap_exit, true} == process_info(whereis(S), trap_exit)
+  end, Shields).
 
 allied(Alliances) ->
-    lists:all(fun ({X, Y}) ->
-                {links, Links} = erlang:process_info(whereis(X), links),
-                YPid = whereis(Y),
-                lists:member(YPid, Links)
-        end, Alliances).
+  lists:all(fun ({X, Y}) ->
+    XPid = whereis(X),
+    ?assert(XPid /= undefined),
+    {links, Links} = erlang:process_info(XPid, links),
+    YPid = whereis(Y),
+    lists:member(YPid, Links)
+  end, Alliances).
 
 validity_printout(Validity) ->
-    Printout = lists:zip([valid_planets, planets_shielded, planets_allied],
-                         tuple_to_list(Validity)),
-    ?debugFmt("Invalid Universe Setup:~n~p~n", [Printout]),
-    false.
+  Printout = lists:zip(
+    [valid_planets, planets_shielded, planets_allied],
+    tuple_to_list(Validity)),
+  ?debugFmt("Invalid Universe Setup:~n~p~n", [Printout]),
+  false.
